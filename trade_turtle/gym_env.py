598be37925dacd6e1614a3d9631e7088e40cebd4
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-17 17:48:58 -0400

    Add Historical Data to OpenAI Gym Env

--- trade_turtle/gym_env.py
+++ trade_turtle/gym_env.py
@@ -3,19 +3,44 @@ from gym import spaces
 import numpy as np
 
 class TradingEnv(gym.Env):
-  def __init__(self):
+  def __init__(self, df):
     super(TradingEnv, self).__init__()
+    self.df = df
+    self.current_step = 0
 
     # define ovservation space (for now, it will be very simple)
     self.action_space = spaces.Discrete(3) # buy, hold, sell
-    self.observation_space = spaces.Box(low=0, high=1, shape=(10,), dtype=np.float32)
+    self.observation_space = spaces.Box(low=0, high=1, shape=(len(df.columns),), dtype=np.float32)
 
   def reset(self):
-    return np.random.rand(10)
+    self.current_step = 0
+    return self._next_observation()
+  
+  def _next_observation(self):
+    obs = self.df.iloc[self.current_step].values
+    return obs
   
   def step(self, action):
-    state = np.random.rand(10)
-    reward = np.random.rand() - 0.5
-    done = False
-    info = {}
-    return state, reward, done, info
+    self.current_step += 1
+
+    obs = self._next_observation()
+
+    reward = self._take_action(action, obs)
+
+    done = self.current_step >= len(self.df) - 1
+
+    return obs, reward, done, {}
+  
+  def _take_action(self, action, obs):
+    # for now, just hardcode some values for buy and sell
+    reward = 0
+
+    if (action > 0.5):
+      reward = -0.5
+    elif (action < -0.5):
+      reward = 1
+
+    return reward
+  
+  def render(self, mode="human", close=False):
+    pass
--- trade_turtle/main.py
+++ trade_turtle/main.py
@@ -1,4 +1,5 @@
 from stable_baselines3 import PPO
+import pandas as pd
 
 from downloader import download_tickers
 from gym_env import TradingEnv
@@ -8,6 +9,11 @@ from gym_env import TradingEnv
 stock_tickers = ["AAPL", "MSFT", "TSLA", "NVDA"]
 download_tickers(stock_tickers)
 
-env = TradingEnv()
+aapl_data = pd.read_csv("./data/AAPL.csv", index_col="Date", parse_dates=True)
+df = aapl_data[["Open", "High", "Low", "Close", "Volume"]]
+# normalize values to range of 0-1
+df = (df - df.min()) / (df.max() - df.min())
+
+env = TradingEnv(df)
 model = PPO("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=10000)
+model.learn(total_timesteps=1_000_000)
