517063282ce76ab509ba5bbe3876a6f4582b3481
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-07-17 16:44:43 -0400

    Make Stock Downloader with YFinance

--- trade_turtle/main.py
+++ trade_turtle/main.py
@@ -1,8 +1,25 @@
 import yfinance as yf
+import pandas as pd
+import os
 
-aapl = yf.Ticker("AAPL")
+# put the tickers you want to download here
+stock_tickers = ["AAPL", "MSFT", "TSLA", "NVDA"]
 
-history = aapl.history()
-last_quote = history['Close'].iloc[-1]
+# how far back to get stock data?
+period="5y"
 
-print("Last Price of AAPL: $" + str(round(last_quote, 2)))
+# create data folder
+if not os.path.exists("./data"):
+  os.mkdir("./data")
+
+# download tickers
+data = yf.download(stock_tickers, period=period, actions=True)
+
+for ticker_name in stock_tickers:
+  ticker_data = data.xs(ticker_name, level=1, axis=1)
+
+  ticker_data.to_csv("./data/" + ticker_name + ".csv")
+
+  last_quote = ticker_data['Close'].iloc[-1]
+
+  print("Last Price of " + ticker_name + ": $" + str(round(last_quote, 2)))
